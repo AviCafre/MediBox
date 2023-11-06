7091c82147365bdff24d63a32ebe107a161ceaeb
Liza Benitez <mar7benitezgmail.com>
2023-11-05 23:16:05 -0300

    terminos y condiciones

---
+++ test.js
@@ -0,0 +1,23 @@
+Swal.fire({
+    html: 'La siguiente informacion es legal y personal. Por favor lea atentamente nuestros <a href="Terminos y Condiciones.pdf">terminos y condiciones.</a>',
+    confirmButtonText: 'Acepto',
+    icon: 'info',
+    padding: '1rem',
+    grow: '1row',
+    backdrop: true,
+    toast: true,
+    position: 'bottom',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    stopKeydownPropagation: false,
+
+    showConfirmButton: true,
+    showCloseButton: false,
+    showCancelButton: false,
+    closeButtonAriaLabel: 'Cerrar esta alerta',
+
+    customClass: {
+        content: 'content-class'
+    }
+
+  });
